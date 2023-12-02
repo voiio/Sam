b7240d1d79af9ee31ab35736e3f5f36e8ec106d0
Johannes Maron <johannes@maron.family>
2023-12-02 13:29:36 +0100

    Resolve #1 -- Add all messages inside a DM, Group, Channel to a Thread

--- sam/slack.py
+++ sam/slack.py
@@ -1,37 +1,53 @@
+import json
 import logging
-import re
 import time
 from typing import Any
 
 from openai import OpenAI
 from slack_bolt import App, Say
 
+from . import config, utils
+
 logger = logging.getLogger("sam")
 
 client = OpenAI()
-
-from . import config, utils
-
-# Event API & Web API
 app = App(token=config.SLACK_BOT_TOKEN)
 
+USER_HANDLE = None
 
-USER_HANDLE = re.compile(r"<@([A-Z0-9]+)>", flags=re.IGNORECASE)
 
-
-def handle_message_events(event: {str, Any}, say: Say):
+def handle_message(event: {str, Any}, say: Say):
+    logger.debug(f"handle_message={json.dumps(event)}")
+    global USER_HANDLE
+    if USER_HANDLE is None:
+        logger.debug("Fetching the bot's user id")
+        response = say.client.auth_test()
+        USER_HANDLE = response["user_id"]
     channel_id = event["channel"]
-    logger.info(
-        f"type={event['type']} client_msg_id={event['client_msg_id']} channel={channel_id} user={event['user']}"
-    )
+    client_msg_id = event["client_msg_id"]
+    channel_type = event["channel_type"]
+    user_id = event["user"]
     text = event["text"]
-    text = USER_HANDLE.sub(r"Sam", text)
+    text = text.replace(f"<@{USER_HANDLE}>", "Sam")
     thread_id = utils.get_thread_id(channel_id)
     client.beta.threads.messages.create(thread_id=thread_id, content=text, role="user")
+    logger.info(
+        f"User={user_id} added Message={client_msg_id} added to Thread={thread_id}"
+    )
+    if channel_type == "im":
+        process_run(event, say)
+
+
+def process_run(event: {str, Any}, say: Say):
+    logger.debug(f"process_run={json.dumps(event)}")
+    channel_id = event["channel"]
+    user_id = event["user"]
+    thread_id = utils.get_thread_id(channel_id)
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=config.OPENAI_ASSISTANT_ID,
     )
+    logger.info(f"User={user_id} started Run={run.id} for Thread={thread_id}")
     cycle = 0
     while run.status in ["queued", "in_progress"]:
         run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
@@ -39,13 +55,18 @@ def handle_message_events(event: {str, Any}, say: Say):
     if run.status != "completed":
         logger.error(run.last_error)
         say(f"🤖 {run.last_error.message}")
+        logger.error(f"Run {run.id} {run.status} for Thread {thread_id}")
+        logger.error(run.last_error.message)
+        return
+    logger.info(f"Run={run.id} {run.status} for Thread={thread_id}")
 
     messages = client.beta.threads.messages.list(thread_id=thread_id)
     for message in messages:
         if message.role == "assistant":
             say(message.content[0].text.value, mrkdwn=True)
+            logger.info(f"Sam responded to the User={user_id} in Channel={channel_id}")
             break
 
 
-app.event("message")(handle_message_events)
-app.event("app_mention")(handle_message_events)
+app.event("message")(handle_message)
+app.event("app_mention")(process_run)
