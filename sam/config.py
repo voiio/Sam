1b8bf23189c1ec45e8d2d6e0bd2c5f2310d60376
Johannes Maron <johannes@maron.family>
2024-04-18 09:55:52 +0200

    Add cost control via the new max_prompt_tokens parameter (#48)
    See also:
    https://platform.openai.com/docs/assistants/how-it-works/max-completion-and-max-prompt-tokens

--- sam/config.py
+++ sam/config.py
@@ -5,6 +5,9 @@ SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
+
+MAX_PROMPT_TOKENS = int(os.getenv("MAX_PROMPT_TOKENS", "2048"))
+
 REDIS_URL = os.getenv("REDIS_URL", "redis:///")
 RANDOM_RUN_RATIO = float(os.getenv("RANDOM_RUN_RATIO", "0"))
 TIMEZONE = ZoneInfo(os.getenv("TIMEZONE", "UTC"))
