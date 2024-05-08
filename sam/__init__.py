d70d0457e6f364cf127d61361c316933301d9747
Rust Saiargaliev <fly.amureki@gmail.com>
2024-05-08 10:34:44 +0200

    Allow importing `bot`, `typing` and `utils`

--- sam/__init__.py
+++ sam/__init__.py
@@ -1,11 +1,15 @@
-"""A GPT based slack bot trained to specific personalities and use cases.
+"""
+A GPT-based Slack bot trained to specific personalities and use cases.
 
 Sam uses OpenAI's assistant API to align ChatGPT to a specific personality traits,
-provide domain specific knowledge and context to provide a work-colleague like
+provide domain-specific knowledge and context to provide a work-colleague like
 experience.
 """
 
-from . import _version
+from . import _version, bot, typing, utils
 
 __version__ = _version.version
 VERSION = _version.version_tuple
+
+
+__all__ = ["__version__", "VERSION", "typing", "utils", "bot"]
